a2e7ca89f31e4f77bf63d690db5ef901d1c81767
Barry Grant <bjgrant@ucsd.edu>
2018-05-14 21:41:47 -0700

    L13 update R script

--- class-material/lecture13_plot.r
+++ class-material/lecture13_plot.r
@@ -1,27 +1,42 @@
+#' ---
+#' output: github_document
+#' ---
 
-#` # Load expresion data from a file or from online
+#' # Plotting Expresion Values
+#' 
+#' This is a complement to the hands on session for lecture 13.
+#' First we need to load our expresion data from a file (that you downloaded) or from online.
 
 #expr <- read.table("rs8067378_ENSG00000172057.6.txt")
 url <- "https://bioboot.github.io/bimm143_S18/class-material/rs8067378_ENSG00000172057.6.txt"
 expr <- read.table(url)
 
-#` Check the genotype sample size and overal summary stats 
+#' Check the genotype sample size and overal summary stats 
 summary(expr)
 
-## Lets break it down to the three genotypes
+#' Lets break it down to the three genotypes by 
+#' 
 summary( expr[expr[,2] == "A/A",3] )
 summary( expr[expr[,2] == "G/G",3] )
 summary( expr[expr[,2] == "A/G",3] )
 
 
-#` Generate some nice plots of our results
+#' We could of also used `expr$geno == "A/A"` in the above, e.g.
+summary( expr$exp[ expr$geno == "A/A" ] )
+
+#' Lets generate some nice plots of our results
 
 # Generate a boxplot for expression data of different genotype groups
 p <- boxplot(exp~geno, data=expr, xlab="rs8067378 genotype", ylab="ENSG00000172057.4 (RPKM)", notch=T)
 
-#` Look at the output 'p' and you will see the median values etc. used to generate the plot
+#' Look at the output 'p' and you will see the median values etc. used to generate the plot
+p
 
-# We could also use the ggplot2 package to make our plots
+#' ## Optional: Using ggplot2
+
+#' We could also use the **ggplot2** package to make our plots
+
+
 library(ggplot2)
 
 ## Boxplot
